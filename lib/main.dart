15be8c84d269001e780cd00342ccb42b9ec18940
Toiletman007-PC <quertical5@gmail.com>
2024-06-07 17:49:09 +0200

    added topbar bottombar a floating button and a drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int count = 0;
+
+  @override
+  void initState() {
+    // TODO: data from firebase
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +34,58 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.red,
           title: const Text("Flutter is Fun!"),
         ),
-        body: Container(
-          child: const Text("Helloooo"),
-          margin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.all(10),
+        body: Stack(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              height: 100,
+              width: 100,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.lightBlue, Colors.red],
+                  stops: [0.2, 0.8],
+                ),
+              ),
+              child: const Text("Helloooo"),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: const EdgeInsets.all(100),
+              padding: const EdgeInsets.all(10),
+              height: 100,
+              width: 100,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.lightBlue, Colors.red],
+                  stops: [0.2, 0.8],
+                ),
+              ),
+              child: const Text("Helloooo"),
+            ),
+            Center(
+              child: Text('count is: $count'),
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              count++;
+            });
+          },
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.business), label: 'Business'),
+            BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
+          ],
+        ),
+        drawer: const Drawer(
+          backgroundColor: Colors.grey,
         ),
       ),
     );
